0ff2e0f439092e8fad880dfa9b025858e609812c
Gareth J. Fentimen <gareth.j.fentimen@avanade.com>
2021-03-26 11:25:49 -0400

    pre code 2

--- src/components/AddToWatchlist.tsx
+++ src/components/AddToWatchlist.tsx
@@ -1,95 +1,77 @@
-import React, {useEffect, useState} from 'react';
-interface SubscriptionInterface {
-    source: any
+import React from 'react';
+
+export interface Stock {
+    symbol: string
 }
 
-export function SubscriptionComponent({source} : SubscriptionInterface) {​
-    const [subscriptionValue, setSubscriptionVal] = useState(null);​
-  
-    useEffect(() => {​
-      const subscription = source.subscribe();​
-  
-      setSubscriptionVal(subscription); ​
-      return () => {​
-        subscription.unsubscribe();​
-      };​
-    }, [source]);​
-  
-    return subscriptionValue ? <div>{subscriptionValue.name}</div> : null;
+export interface OnAddFunction {
+    (stockAdded: Stock) : void
 }
 
-// export interface Stock {
-//     symbol: string
-// }
+export interface OnRemoveFunction {
+    (stockAdded: Stock) : void
+}
 
-// export interface OnAddFunction {
-//     (stockAdded: Stock) : void
-// }
+export interface AddToWatchlistProps {
+    onAdd: OnAddFunction,
+    onRemove: OnRemoveFunction
+}
 
-// export interface OnRemoveFunction {
-//     (stockAdded: Stock) : void
-// }
+function AddToWatchlist({ onAdd, onRemove } : AddToWatchlistProps) {
+    const [stock, setStock] = React.useState<string>("");
+    const addToWatchlist = async () => {
+        const response = await fetch("https://demomocktradingserver.azurewebsites.net/userdata/watchlist",
+            {
+                body: JSON.stringify({
+                    symbol: stock,
+                    action: "ADD"
+                }),
+                method: 'POST',
+                headers: { 
+                    userid: "gareth.fentimen" 
+                }
+            }
+        );
 
-// export interface AddToWatchlistProps {
-//     onAdd: OnAddFunction,
-//     onRemove: OnRemoveFunction
-// }
+        const jsonResponse = await response.json();
+        jsonResponse.success && onAdd({symbol: stock});
+    };
 
-// function AddToWatchlist({ onAdd, onRemove } : AddToWatchlistProps) {
-//     const [stock, setStock] = React.useState<string>("");
-//     const addToWatchlist = async () => {
-//         const response = await fetch("https://demomocktradingserver.azurewebsites.net/userdata/watchlist",
-//             {
-//                 body: JSON.stringify({
-//                     symbol: stock,
-//                     action: "ADD"
-//                 }),
-//                 method: 'POST',
-//                 headers: { 
-//                     userid: "gareth.fentimen" 
-//                 }
-//             }
-//         );
+    const removeFromWatchlist = async () => {
+        const response = await fetch("https://demomocktradingserver.azurewebsites.net/userdata/watchlist",
+            {
+                body: JSON.stringify({
+                    symbol: stock,
+                    action: "REMOVE"
+                }),
+                method: 'POST',
+                headers: { 
+                    userid: "gareth.fentimen" 
+                }
+            }
+        );
 
-//         const jsonResponse = await response.json();
-//         jsonResponse.success && onAdd({symbol: stock});
-//     };
+        const jsonResponse = await response.json();
+        jsonResponse.success && onRemove({symbol: stock});
+    };
 
-//     const removeFromWatchlist = async () => {
-//         const response = await fetch("https://demomocktradingserver.azurewebsites.net/userdata/watchlist",
-//             {
-//                 body: JSON.stringify({
-//                     symbol: stock,
-//                     action: "REMOVE"
-//                 }),
-//                 method: 'POST',
-//                 headers: { 
-//                     userid: "gareth.fentimen" 
-//                 }
-//             }
-//         );
+    const handleAdd = (e: any) => {
+        e.preventDefault();
+        addToWatchlist();
+    }
 
-//         const jsonResponse = await response.json();
-//         jsonResponse.success && onRemove({symbol: stock});
-//     };
-
-//     const handleAdd = (e: any) => {
-//         e.preventDefault();
-//         addToWatchlist();
-//     }
-
-//     const handleRemove = (e: any) => {
-//         e.preventDefault();
-//         removeFromWatchlist();
-//     }
+    const handleRemove = (e: any) => {
+        e.preventDefault();
+        removeFromWatchlist();
+    }
       
-//     return (<div>
-//         <form onSubmit={(e: any) => e.preventDefault()}>
-//             <input type={'text'} id={'stock'} value={stock} onChange={(e: any) => { setStock(e.target.value); }} />
-//             <button onClick={handleAdd}>Add stock to watchlist</button>
-//             <button onClick={handleRemove}>Remove stock from watchlist</button>
-//         </form>
-//       </div>);
-// };
+    return (<div>
+        <form onSubmit={(e: any) => e.preventDefault()}>
+            <input type={'text'} id={'stock'} value={stock} onChange={(e: any) => { setStock(e.target.value); }} />
+            <button onClick={handleAdd}>Add stock to watchlist</button>
+            <button onClick={handleRemove}>Remove stock from watchlist</button>
+        </form>
+      </div>);
+};
 
-// export default AddToWatchlist;
+export default AddToWatchlist;
